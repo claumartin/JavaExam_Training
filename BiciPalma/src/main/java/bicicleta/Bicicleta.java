eb28e7a3ab9c428e417c4329483426eda8f6c183
unknown <claudiamariamail97@gmail.com>
2019-03-02 16:24:59 +0100

    Implemented  Bicicleta

--- BiciPalma/src/main/java/bicicleta/Bicicleta.java
+++ BiciPalma/src/main/java/bicicleta/Bicicleta.java
@@ -1,3 +1,22 @@
 package bicicleta;
 
-public class Bicicleta {}
+public class Bicicleta {
+
+    // Atributos
+
+    int idBicicleta = 0;
+
+    // Constructores
+
+    public Bicicleta() {}
+
+    public Bicicleta(int idBicicleta) {
+        this.idBicicleta = idBicicleta;
+    }
+
+    // Getter
+
+    public int getIdBicicleta() {
+        return this.idBicicleta;
+    }
+}
--- BiciPalma/src/test/java/bicicletaTest/BicicletaTest.java
+++ BiciPalma/src/test/java/bicicletaTest/BicicletaTest.java
@@ -1,3 +1,25 @@
 package bicicletaTest;
 
-public class BicicletaTest {}
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import bicicleta.Bicicleta;
+
+public class BicicletaTest {
+
+    Bicicleta bicicletaTest = new Bicicleta(9);
+
+    @Before
+    public void init() {
+        Bicicleta bicicletaTest = new Bicicleta(9);
+        assertNotNull(bicicletaTest);
+    }
+
+    @Test
+    public void getIdBicicletaTest() {
+        assertEquals(9, bicicletaTest.getIdBicicleta());
+    }
+}
