deb11e8d4436a0296cb917aa0a9eeac73eea9fc5
unknown <claudiamariamail97@gmail.com>
2019-03-02 16:15:52 +0100

    consultarEstacion() done

--- BiciPalma/src/main/java/estacion/Estacion.java
+++ BiciPalma/src/main/java/estacion/Estacion.java
@@ -1,3 +1,42 @@
 package estacion;
 
-public class Estacion {}
+public class Estacion {
+
+    // Atributos
+
+    int idEstacion = 0;
+    String direccion = "";
+    int anclajes = 0;
+
+    // Constructor
+
+    public Estacion() {}
+
+    public Estacion(int idEstacion, String direccion, int anclajes) {
+        this.idEstacion = idEstacion;
+        this.direccion = direccion;
+        this.anclajes = anclajes;
+    }
+
+    // Getters
+
+    public int getIdEstacion() {
+        return this.idEstacion;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public int getAnclajes() {
+        return this.anclajes;
+    }
+
+    // Lógica
+    public void consultarEstacion() {
+        System.out.println("\n"+ "Id Estación: " + getIdEstacion() + "\n" + 
+                            "Dirección: " + getDireccion() + "\n" + 
+                            "Anclaje: " + getAnclajes());
+    }
+
+}
--- BiciPalma/src/test/java/biciPalmaTest/BiciPalmaTest.java
+++ BiciPalma/src/test/java/biciPalmaTest/BiciPalmaTest.java
@@ -2,6 +2,33 @@ package biciPalmaTest;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class BiciPalmaTest {}
+import estacion.Estacion;
+
+public class BiciPalmaTest {
+
+    Estacion estacionTest = new Estacion(1, "Manacor", 6);
+
+    @Before
+    public void init() {
+        Estacion estacionTest = new Estacion(1, "Manacor", 6);
+        assertNotNull(estacionTest);
+    }
+
+    @Test
+    public void getIdEstacionTest() {
+        assertEquals(1, estacionTest.getIdEstacion());
+    }
+
+    @Test
+    public void getDireccionTest() {
+        assertEquals("Manacor", estacionTest.getDireccion());
+    }
+
+    @Test
+    public void getAnclajesTest() {
+        assertEquals(6, estacionTest.getAnclajes());
+    }
+}
