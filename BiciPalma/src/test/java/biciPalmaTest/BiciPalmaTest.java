be926f1057280be6965bf3f57321d26f56db9dad
unknown <claudiamariamail97@gmail.com>
2019-03-02 16:29:24 +0100

    Passed Estacion test cases to its own file

--- BiciPalma/src/test/java/biciPalmaTest/BiciPalmaTest.java
+++ BiciPalma/src/test/java/biciPalmaTest/BiciPalmaTest.java
@@ -1,34 +1,3 @@
 package biciPalmaTest;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import estacion.Estacion;
-
-public class BiciPalmaTest {
-
-    Estacion estacionTest = new Estacion(1, "Manacor", 6);
-
-    @Before
-    public void init() {
-        Estacion estacionTest = new Estacion(1, "Manacor", 6);
-        assertNotNull(estacionTest);
-    }
-
-    @Test
-    public void getIdEstacionTest() {
-        assertEquals(1, estacionTest.getIdEstacion());
-    }
-
-    @Test
-    public void getDireccionTest() {
-        assertEquals("Manacor", estacionTest.getDireccion());
-    }
-
-    @Test
-    public void getAnclajesTest() {
-        assertEquals(6, estacionTest.getAnclajes());
-    }
-}
+public class BiciPalmaTest {}
--- BiciPalma/src/test/java/estacionTest/EstacionTest.java
+++ BiciPalma/src/test/java/estacionTest/EstacionTest.java
@@ -1,3 +1,34 @@
 package estacionTest;
 
-public class EstacionTest {}
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import estacion.Estacion;
+
+public class EstacionTest {
+
+    Estacion estacionTest = new Estacion(1, "Manacor", 6);
+
+    @Before
+    public void init() {
+        Estacion estacionTest = new Estacion(1, "Manacor", 6);
+        assertNotNull(estacionTest);
+    }
+
+    @Test
+    public void getIdEstacionTest() {
+        assertEquals(1, estacionTest.getIdEstacion());
+    }
+
+    @Test
+    public void getDireccionTest() {
+        assertEquals("Manacor", estacionTest.getDireccion());
+    }
+
+    @Test
+    public void getAnclajesTest() {
+        assertEquals(6, estacionTest.getNumeroAnclajes());
+    }
+}
